01c7f54a8eb248e34db85cea80be227360b0f1c8
hanxia <hanxia@gmail.com>
2016-06-24 17:28:19 +0800

    打印log

--- app/src/main/java/cn/peapad/avsdk/testndk/MainActivity.java
+++ app/src/main/java/cn/peapad/avsdk/testndk/MainActivity.java
@@ -2,10 +2,12 @@ package cn.peapad.avsdk.testndk;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-private TextView textView;
+    private static final String TAG ="MainActivity" ;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +15,7 @@ private TextView textView;
         textView=(TextView)findViewById(R.id.text);
         Test test=new Test();
         textView.setText(test.helloC());
+        Log.i(TAG,"AHAHAHHA");
 
     }
 }
